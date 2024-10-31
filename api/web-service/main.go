f24a1d5112470eada1062eaa87fede8fb148e570
Scott Bolinger <designbyscott@gmail.com>
2024-10-31 07:22:56 -0700

    add get product by id

--- api/web-service/main.go
+++ api/web-service/main.go
@@ -34,10 +34,23 @@ func postProducts( c *gin.Context) {
   c.IndentedJSON(http.StatusCreated, newProduct)
 }
 
+func getProductByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a:= range products {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
 func main() {
   router := gin.Default()
   router.Use(cors.Default())
   router.GET("/products", getProducts)
+  router.GET("/products/:id", getProductByID)
   router.POST("/products", postProducts)
 
   router.Run(":80")
